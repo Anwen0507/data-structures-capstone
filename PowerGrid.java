f0eb39d9afdd5b6472d36601aca3cb8d18d49ad7
Anwen Hao <haoanwen@gmail.com>
2025-12-09 17:21:34 -0500

    Begin Prim's MST Algorithm

--- PowerGrid.java
+++ PowerGrid.java
@@ -1,11 +1,24 @@
+import java.util.Comparator;
+import java.util.List;
+
 public class PowerGrid {
     private class Edge {
-        private int end, weight;
+        private int start, end, weight;
         private String name;
-        private Edge(int end, int weight, String name) {
+
+        private Edge(int start, int end, int weight, String name) {
+            this.start = start;
             this.end = end;
             this.weight = weight;
             this.name = name;
         }
+    }
+
+    public List<Edge> mst(List<List<Edge>> graph) {
+        Comparator<Edge> comparator = (x, y) -> {
+            return x.weight != y.weight
+                ? x.weight - y.weight
+                : x.start - y.start;
+        };
     }
 }
